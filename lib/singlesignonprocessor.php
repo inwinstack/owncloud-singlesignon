788aca951a0abc02e704be9a30d08388adb18d6e
DabuaKao <dauba.k@inwinstack.com>
2016-01-20 15:42:46 +0800

    check SSO config exist in SingleSingnOnProcessor::construct()

--- lib/singlesignonprocessor.php
+++ lib/singlesignonprocessor.php
@@ -5,15 +5,51 @@ use Exception;
 
 class SingleSignOnProcessor {
 
-    private $token;
-    private $ssoconfig;
-    private $userIp;
-    private $redirectUrl;
-    private $soapClient;
-    private $hostIp;
-    private $hostDomainName;
-    private $request;
+    /**
+     * required keys in config/config.php
+     */
+    private static $requiredKeys = array("sso_login_url",
+                                        "sso_auth_method",
+                                        "sso_return_url_key",
+                                        "sso_requests",
+                                        "sso_portal_url",
+                                        "sso_global_logout");
 
+    /**
+     * \OC\SystemConfig
+     */
+    private $config;
+
+    /**
+     * \OC\Appframework\Http\Request
+     */
+    private $request; 
+
+    /**
+     * user token
+     */
+    private $token;
+
+    /**
+     * user ip
+     */
+    private $userIp;
+
+    /**
+     * url where to redirect after SSO login
+     */
+    private $redirectUrl;
+
+    /**
+     * soap client object
+     */
+    private $soapClient;
+
+    /**
+     * SSO auth method
+     */ 
+    private $authMathod;
+    
     public function run() {
         try {
             $this->process();
@@ -23,20 +59,33 @@ class SingleSignOnProcessor {
     }
 
     public function __construct() {
+        $this->config = \OC::$server->getSystemConfig();
+        $this->authMathod = $this->config->getValue("sso_auth_method");
+
+        if ($this->authMathod === "param") {
+            array_push(self::$requiredKeys, "sso_url_token_key");
+        }
+        else {
+            array_push(self::$requiredKeys, "sso_cookie_token_key");
+        }
+
+        self::checkKeyExist(self::$requiredKeys);
+        self::checkConfigValueEmpty(self::$requiredKeys);
+
         $this->request = \OC::$server->getRequest();
-        $this->ssoconfig = \OC::$server->getSystemConfig()->getValue("SSOCONFIG");
         $this->userIp = $this->request->getRemoteAddress();
         $this->redirectUrl = \OC_Util::getDefaultPageUrl();
-        $this->token = $this->request->offsetGet($this->ssoconfig["urlToken"]) | \OC::$server->getSession()->get("sso_token") | $this->request->getCookie($this->ssoconfig["token"]);
-        RequestManager::init("soap", $this->ssoconfig["singleSignOnServer"], $this->ssoconfig["requests"]);
+        $this->token = $this->request->offsetGet($this->config->getValue("sso_url_token_key")) | \OC::$server->getSession()->get("sso_token") | $this->request->getCookie($this->config->getValue("sso_cookie_token_key"));
+
+        RequestManager::init("soap", $this->config->getValue("sso_portal_url"), $this->config->getValue("sso_requests"));
     }
 
     public function process() {
-        $ssoUrl = $this->ssoconfig["ssoUrl"];
+        $ssoUrl = $this->config->getValue("sso_login_url");
         $userInfo = RequestManager::getRequest(ISingleSignOnRequest::INFO);
 
         if(isset($_GET["logout"]) && $_GET["logout"] == "true") {
-            if($this->ssoconfig["logoutSSO"]) {
+            if($this->config->getValue("sso_global_logout")) {
                 RequestManager::send(ISingleSignOnRequest::INVALIDTOKEN);
             }
             \OC_User::logout();
@@ -56,9 +105,7 @@ class SingleSignOnProcessor {
 
         if(\OC_User::isLoggedIn() && ($this->token === false || !RequestManager::send(ISingleSignOnRequest::VALIDTOKEN, array("token" => $this->getToken(), "userIp" => $this->getUserIp())))) {
             header("HTTP/1.1 " . \OCP\AppFramework\Http::STATUS_UNAUTHORIZED);
-            header("Status: " . \OCP\AppFramework\Http::STATUS_UNAUTHORIZED);
-            header("WWW-Authenticate: ");
-            header("Retry-After: 120");
+            header("Status: " . \OCP\AppFramework\Http::STATUS_UNAUTHORIZED); header("WWW-Authenticate: "); header("Retry-After: 120");
 
             $template = new \OC_Template("singlesignon", "tokenExpired", "guest");
             $template->printPage();
@@ -66,7 +113,7 @@ class SingleSignOnProcessor {
         }
 
         if($this->getToken() === false || !RequestManager::send(ISingleSignOnRequest::VALIDTOKEN, array("token" => $this->getToken(), "userIp" => $this->getUserIp()))) {
-            $url = ($this->redirectUrl) ? $ssoUrl . $this->ssoconfig["returnUrl"] . $this->redirectUrl : $ssoUrl;
+            $url = ($this->redirectUrl) ? $ssoUrl . $this->config->getValue("sso_return_url_key") . $this->redirectUrl : $ssoUrl;
             Util::redirect($url);
         }
 
@@ -88,14 +135,64 @@ class SingleSignOnProcessor {
         }
     }
 
+    /**
+     * Check key is exist or not in config/config.php
+     *
+     * @param array reqiured keys
+     * @return void
+     */
+    public static function checkKeyExist($requiredKeys) {
+        $configKeys = \OC::$server->getSystemConfig()->getKeys();
+
+        foreach ($requiredKeys as $key) {
+            if (!in_array($key, $configKeys)) {
+                throw new Exception("The config key " . $key . " did't exist.");
+            }
+        }
+    }
+
+    /**
+     * Check config value is empty or not.
+     *
+     * @param array reqiured keys
+     * @return void
+     **/
+    public static function checkConfigValueEmpty($requiredKeys) {
+        $config = \OC::$server->getSystemConfig();
+
+        foreach ($requiredKeys as $key) {
+            if (!$config->getValue($key)) {
+                throw new Exception("The config value " . $key . " is empty.");
+            }
+        }
+    }
+
+    /**
+     * Get SingleSignOnProcessor.
+     *
+     * @return Object \OCA\SingleSigoOnProcessor
+     * @author Dauba
+     */
     public static function getInstance() {
         return new static();
     }
 
+    /**
+     * Get the user token
+     *
+     * @return string user token
+     * @author Dauba
+     */
     public function getToken() {
         return $this->token;
     }
     
+    /**
+     * Get the user ip
+     *
+     * @return string user ip
+     * @author Dauba
+     */
     public function getUserIp() {
         return $this->userIp;
     }
