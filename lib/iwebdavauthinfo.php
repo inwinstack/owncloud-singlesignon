eb13c9d6b3a97eae5fd65dcb8dee47ea47bb3ae0
DaubaKao <dauba.k@inwinstack.com>
2016-04-26 16:49:14 +0800

    [update] remove unnecessary method from IWevDavAuthInfo

--- lib/iwebdavauthinfo.php
+++ lib/iwebdavauthinfo.php
@@ -9,20 +9,10 @@ namespace OCA\SingleSignOn;
 interface IWebDavAuthInfo
 {
     /**
-     * set auth info
-     *
-     * @param string $userID
-     * @param string $password
-     * @return void
-     * @author Dauba
-     */
-    public static function init($userID, $password);
-
-    /**
      * get auth info
      *
      * @return array
      * @author Dauba
      */
-    public static function get();                                                                                                              
+    public static function get($userID, $password);
 }
